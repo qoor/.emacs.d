c797e80d4f2b7bb4657deb2e072029a2e3e444db
Qoo <akck1202@hanmail.net>
2021-05-11 09:27:04 +0900

    Add my C/C++ style

--- init.el
+++ init.el
@@ -68,21 +68,81 @@
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
-(add-hook 'c-mode-common-hook 'display-fill-column-indicator-mode)
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (display-fill-column-indicator-mode)
-            (c-set-offset 'access-label '/)
-            (c-set-offset 'case-label '+)
-            (c-set-offset 'innamespace '0)
-            (c-set-offset 'inline-open '0)
-            (c-set-offset 'inextern-lang '0)))
+(defconst my-c-style
+  `((c-recognize-knr-p . nil)
+    (c-enable-xemacs-performance-kludge-p . t) ; speed up indentation in XEmacs
+    (c-basic-offset . 4)
+    (indent-tabs-mode . nil)
+    (c-comment-only-line-offset . 0)
+    (c-hanging-braces-alist . ((defun-open after)
+                               (defun-close before after)
+                               (class-open after)
+                               (class-close before after)
+                               (inexpr-class-open after)
+                               (inexpr-class-close before)
+                               (namespace-open after)
+                               (inline-open after)
+                               (inline-close before after)
+                               (block-open after)
+                               (block-close . c-snug-do-while)
+                               (extern-lang-open after)
+                               (extern-lang-close after)
+                               (statement-case-open after)
+                               (substatement-open after)))
+    (c-hanging-colons-alist . ((case-label)
+                               (label after)
+                               (access-label after)
+                               (member-init-intro before)
+                               (inher-intro)))
+    (c-hanging-semi&comma-criteria
+     . (c-semi&comma-no-newlines-for-oneline-inliners
+        c-semi&comma-inside-parenlist
+        c-semi&comma-no-newlines-before-nonblanks))
+    (c-indent-comments-syntactically-p . t)
+    (comment-column . 40)
+    (c-indent-comment-alist . ((other . (space . 2))))
+    (c-cleanup-list . (brace-else-brace
+                       brace-elseif-brace
+                       brace-catch-brace
+                       empty-defun-braces
+                       defun-close-semi
+                       list-close-comma
+                       scope-operator))
+    (c-offsets-alist . ((func-decl-cont . ++)
+                        (member-init-intro . ++)
+                        (inher-intro . ++)
+                        (comment-intro . 0)
+                        ;; (arglist-close . c-lineup-arglist)
+                        (topmost-intro . 0)
+                        (block-open . 0)
+                        (inline-open . 0)
+                        (substatement-open . 0)
+                        (statement-cont
+                         .
+                         (,(when (fboundp 'c-no-indent-after-java-annotations)
+                             'c-no-indent-after-java-annotations)
+                          ,(when (fboundp 'c-lineup-assignments)
+                             'c-lineup-assignments)
+                          ++))
+                        (label . /)
+                        (case-label . +)
+                        (statement-case-intro . +) ; case w/o {
+                        (access-label . /)
+                        (innamespace . 0))))
+  "My C/C++ Programming Style.")
+
+(c-add-style "my-c-style" my-c-style)
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             (display-fill-column-indicator-mode)
             (flycheck-mode t)))
 (add-hook 'makefile-mode-hook 'my-makefile-config)
 (add-hook 'makefile-gmake-mode-hook 'my-makefile-config)
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (display-fill-column-indicator-mode)
+            (c-set-style "my-c-style")))
 
 (use-package dracula-theme
   :config
